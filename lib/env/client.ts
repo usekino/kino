26784605a1de946f86d66c9f29fd8a7113a5db53
Nate Dunn <hello@natedunn.net>
2024-04-10 12:11:44 -0600

    fix: clean up env again

--- lib/env/client.ts
+++ lib/env/client.ts
@@ -8,15 +8,8 @@ export const env = createEnv({
 		// Project
 		NEXT_PUBLIC_ROOT_DOMAIN: z.string().min(1),
 	},
-	shared: {
-		// Node
-		NODE_ENV: z.enum(['development', 'test', 'production']),
-		PORT: z.coerce.number().default(3000),
-	},
-	runtimeEnv: {
+	experimental__runtimeEnv: {
 		NEXT_PUBLIC_ROOT_DOMAIN: process.env.NEXT_PUBLIC_ROOT_DOMAIN,
-		NODE_ENV: process.env.NODE_ENV,
-		PORT: process.env.PORT,
 	},
 	isServer: typeof window === 'undefined',
 });
--- lib/env/server.ts
+++ lib/env/server.ts
@@ -4,13 +4,16 @@ import { z } from 'zod';
 
 export const env = createEnv({
 	extends: [vercel],
+	client: {
+		// Project
+		NEXT_PUBLIC_ROOT_DOMAIN: z.string().min(1),
+	},
 	server: {
 		DATABASE_URL: z.string().url().min(1),
 		RESEND_API_KEY: z.string().min(1),
 	},
-	runtimeEnv: {
-		DATABASE_URL: process.env.DATABASE_URL,
-		RESEND_API_KEY: process.env.RESEND_API_KEY,
+	experimental__runtimeEnv: {
+		NEXT_PUBLIC_ROOT_DOMAIN: process.env.NEXT_PUBLIC_ROOT_DOMAIN,
 	},
 	isServer: typeof window === 'undefined',
 });
