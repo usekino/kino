2a641f551030acbaf1eaf803d6224622f7fb6444
Nate Dunn <hello@natedunn.net>
2024-05-27 13:57:16 -0600

    add test loading state

---
+++ app/app/~/[team]/[project]/loading.tsx
@@ -0,0 +1,7 @@
+export default async function LoadingProjectPage() {
+	return (
+		<div className='flex h-screen w-full flex-col items-center justify-center gap-2'>
+			<h1 className='text-6xl font-bold'>Loading...</h1>
+		</div>
+	)
+}
