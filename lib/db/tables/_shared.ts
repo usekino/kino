67791f2f77a40d0bf749e207c6f6327400be3fa6
Nate Dunn <hello@natedunn.net>
2025-01-27 09:46:27 -0600

    Don't use $onUpdate method anymore cause it doesn't work

--- lib/db/tables/_shared.ts
+++ lib/db/tables/_shared.ts
@@ -4,6 +4,7 @@ import { integer, serial, timestamp, varchar } from 'drizzle-orm/pg-core';
 export const defaults = {
 	randomId: sql`LPAD(LEFT(REPLACE(REPLACE(REPLACE(encode(convert_to(md5(random()::text), 'utf-8'), 'base64'), '/', ''), '+', ''), '=', ''), 15), 15, '0')`,
 	currentTimestamp: sql`CURRENT_TIMESTAMP`,
+
 };
 
 export const defaultColumns = () => {
@@ -11,13 +12,7 @@ export const defaultColumns = () => {
 		autoId: serial('auto_id').notNull().primaryKey(),
 		id: varchar('id', { length: 255 }).unique().default(defaults.randomId).notNull(),
 		createdAt: timestamp('created_at').default(defaults.currentTimestamp).notNull(),
-		updatedAt: timestamp('updated_at')
-			.default(defaults.currentTimestamp)
-			.notNull()
-			.$onUpdate(() => {
-				console.log('updatedAt??');
-				return sql`CURRENT_TIMESTAMP`;
-			}),
+		updatedAt: timestamp('updated_at').default(defaults.currentTimestamp).notNull(),
 		deletedAt: timestamp('deleted_at'),
 		updates: integer('updates').default(0).notNull(),
 	};
